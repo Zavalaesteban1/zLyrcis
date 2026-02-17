6224b351aa8635d4a4baa324cdd1fe1430789d0d
Esteban Zavala <zavala_esteban4455@yahoo.com>
2026-02-17 16:40:59 -0600

    devugging

--- backend/lyric_video_project/settings.py
+++ backend/lyric_video_project/settings.py
@@ -98,6 +98,7 @@ WSGI_APPLICATION = 'lyric_video_project.wsgi.application'
 import dj_database_url
 
 DATABASE_URL = os.environ.get('DATABASE_URL')
+print("=========Debuging the backend in production =======")
 print(f"DEBUG: DATABASE_URL repr = {repr(DATABASE_URL)}")
 print(f"DEBUG: DATABASE_URL length = {len(DATABASE_URL)}")
 
